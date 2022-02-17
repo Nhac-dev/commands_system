9415eb79e5f4a8fedc2b056e0d040f69c858b304
NhacDev <nhac.developer@gmail.com>
2022-02-17 16:05:23 -0300

    Add lj, equal to ls

--- src/lj.cpp
+++ src/lj.cpp
@@ -12,15 +12,15 @@ const char *command_exec = NULL;
 int main(int argc, char* argv[]){
     // Change {dir} to dir target
     // Example C:/Users/my-profille/Desktop/commands
-    char cmd_x[100] = "node C:/Users/Jefferson/Desktop/my-commands/src/js/touch.js ";
-    
+    char cmd_x[100] = "node C:/Users/Jefferson/Desktop/my-commands/src/js/ls.js ";
+
     if(argv[1]){
         command_exec = strcat(cmd_x, argv[1]);
-        system(command_exec);
     }else{
-        cout << "Argumento não passado! Erro ao completar o comando!";
+        command_exec = cmd_x;
     }
-
+    
+    system(command_exec);
 
     return 0;
 }
